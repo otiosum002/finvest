9634456bd004e2115646f4e3925c6212a8376c31
Ak <otiosum002@gmail.com>
2025-01-21 12:54:01 +0530

    removing picture

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,17 +19,6 @@ const Contact = () => {
   return (
     <div id="contact" className="my-20 min-h-96 w-screen px-10">
       <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
-        <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
-          <ImageClipBox
-            src="/img/swordman-partial.webp"
-            clipClass="absolute md:scale-125"
-          />
-          <ImageClipBox
-            src="/img/swordman.webp"
-            clipClass="sword-man-clip-path md:scale-125"
-          />
-        </div>
-
         <div className="flex flex-col items-center text-center">
           <p className="mb-10 font-general text-[10px] uppercase">Join Mint Money</p>
 
